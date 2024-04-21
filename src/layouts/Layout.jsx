76288bfb6dd2dd9336b7740fbbb05991b36ecac1
ahmakbardev <ahmakbar.dev@gmail.com>
2024-04-21 15:53:18 +0700

    add temporary sidebar for make navigation easier on development

--- src/layouts/Layout.jsx
+++ src/layouts/Layout.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
+import TempSidebar from "../components/TempSidebar";
 
 const Layout = ({ children }) => {
   return (
     <>
       <Header />
+      <TempSidebar />
       {children}
       <Footer />
     </>
